e5ffd6b3b1cd10bad64706b7c65b6d61208e8861
pdevq <45507854+pdevq@users.noreply.github.com>
2021-02-27 20:29:47 -0500

    [bug] Here Doc not properly parsed  - line 10-13 should be considered string with parameter substitution  - line 11 ' wrongly considered as opening a string without para sub  - line 12-18 are now shown as plain string with a closing ' in the comment  - line 22 should be displayed as string without parameter substitution

--- test.sh
+++ test.sh
@@ -6,10 +6,22 @@ IFS=$ifs_old
 
 # some comment'
 
+function help() { cat; }<<EOF_HERE
+Some text and $variable
+This doesn't seem right.
+Everything is now considered a string due to the unmatched single quote.
+Even $variables.
+EOF_HERE
+
 today=$(date --iso-8601)
 
-echo "Today is: $today"
+echo "Today is: $today" # we can again fix this with another fake closing quote '
 echo "Today is: $(date --iso-8601)"
+
+cat <<'SOME_TOKEN'
+This $variable should be displayed as string
+SOME_TOKEN
+
 echo $today
 
 ls -lah
